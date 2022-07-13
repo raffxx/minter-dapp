5ec3ae014981ed6c48fa5abcbc5ee29746501e7b
raffxx <you@example.com>
2022-07-13 16:51:22 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x1D4a64B4faD57426A1Ecd6Ff15784044Ebea302a"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
